e1ef3cda3baf8c4534c64852dfa7dc2548c9b99f
dev-paulocesar1 <paulo.programacao3625@gmail.com>
2024-03-07 23:30:32 -0300

    feat: add SearchForm api connection

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -29,10 +29,15 @@ export const SearchFormContainer = styled.form`
     font-weight: bold;
     border-radius: 6px;
 
-    &:hover {
-      background: ${({ theme }) => theme['green-500']};
-      border: 1px solid ${({ theme }) => theme['green-500']};
-      color: ${({ theme }) => theme.white};
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
+      background: ${(props) => props.theme['green-500']};
+      border-color: ${(props) => props.theme['green-500']};
+      color: ${(props) => props.theme.white};
       transition:
         background-color 0.2s,
         color 0.2s,
